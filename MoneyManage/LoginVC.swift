ba9e2cad73c75fbcc05b0fc6698db7ccb355ea1a
Rahmatulloh <socialkavinsky@gmail.com>
2022-07-05 15:32:23 +0300

    Added Animations to sign button

--- MoneyManage/LoginVC.swift
+++ MoneyManage/LoginVC.swift
@@ -146,11 +146,11 @@ extension LoginVC {
             assertionFailure("username / password should never be nil")
             return
         }
-//        if username.isEmpty || password.isEmpty {
-//            configureView(withMessage: "Username / password cannot be blank!")
-//            return
-//        }
-        if username == "" && password == "" {
+        if username.isEmpty || password.isEmpty {
+            configureView(withMessage: "Username / password cannot be blank!")
+            return
+        }
+        if username == "Kenan" && password == "welcome" {
             signInButton.configuration?.showsActivityIndicator = true
             delegate?.didLogin()
             
@@ -161,6 +161,17 @@ extension LoginVC {
     private func configureView(withMessage message: String) {
         errorMessageLabel.isHidden = false
         errorMessageLabel.text = message
+        shakeButton()
+    }
+    private func shakeButton() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+
+        animation.isAdditive = true
+        signInButton.layer.add(animation, forKey: "shake")
     }
 }
 extension LoginVC {
